0dfbd5b65c5902e24678b21ef36d7d3ac96cae11
Lorenzo Pantano <l.pantano97@gmail.com>
2026-02-27 22:33:13 +0100

    chore: remove build folder

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lorenzopantano/Developer/lowlevel/ctoml/tests
-# Build directory: /Users/lorenzopantano/Developer/lowlevel/ctoml/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[basic_first_test]=] "/Users/lorenzopantano/Developer/lowlevel/ctoml/build/tests/test_ctoml")
-set_tests_properties([=[basic_first_test]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/lorenzopantano/Developer/lowlevel/ctoml/tests/CMakeLists.txt;3;add_test;/Users/lorenzopantano/Developer/lowlevel/ctoml/tests/CMakeLists.txt;0;")
